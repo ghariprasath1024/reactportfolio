f6f92dbab9c80629009d5eb51f94e811e5cf5c63
ghariprasath1024 <ghariprasath10242gmail.com>
2026-07-18 08:09:45 +0530

    added

--- src/Portfolio/Skill.jsx
+++ src/Portfolio/Skill.jsx
@@ -14,7 +14,7 @@ const Skill = () => {
                     </div>
                     <div className='Foodercontent2'>
                         <h3>Projects</h3><hr />
-                        <a>to be continued...</a>
+                        <a href='https://example.com'>projects024.netlify.app</a>
                     </div>
                     <div className='Foodercontent2'>
                         <h3>AI Portfolio</h3><hr />
